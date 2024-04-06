e04ee41cfa5a62c2898c7838ee80004bba280faf
toey-arch <saranyatongmee.2000@gmail.com>
2024-04-07 02:41:20 +0700

    Add temp model

--- src/Thesis/temp2.js
+++ src/Thesis/temp2.js
@@ -1,9 +1,15 @@
 import * as THREE from 'three';
 import Stats from 'three/addons/libs/stats.module.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import {
+    OrbitControls
+} from 'three/addons/controls/OrbitControls.js';
+import {
+    GLTFLoader
+} from 'three/addons/loaders/GLTFLoader.js';
 
+import {
+    GUI
+} from 'three/addons/libs/lil-gui.module.min.js';
 
 class Thesis {
     constructor() {
@@ -31,7 +37,9 @@ class Thesis {
     }
 
     initRenderer() {
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -41,8 +49,11 @@ class Thesis {
         // this.scene.background = new THREE.Color(0xbfe3dd);
         this.scene.background = new THREE.Color(0x87CEEB);
 
-        let envLight = new THREE.HemisphereLight(0xffffff,0xa88132,3);
-        envLight.position.set(0, 17, 0)
+        let axisHelper = new THREE.AxesHelper(100);
+        this.scene.add(axisHelper);
+
+        let envLight = new THREE.HemisphereLight(0xffffff, 0xa88132, 3);
+        envLight.position.set(0, 100, 0)
         // let ambientLight = new THREE.AmbientLight(0x000000, 3)
         // ambientLight.position.set(0, 10 ,0) 
         // this.scene.add(ambientLight)
@@ -51,55 +62,55 @@ class Thesis {
 
         //this.scene.add(envLight);
 
-        let geometry = new THREE.BoxGeometry(2,2,2);
-        let material = new THREE.MeshStandardMaterial( 
-            {
-                color:0x00ff00, 
-                emissive: 0x0,
-                transparent: false,
-                opacity: 0,
-                depthTest: true,
-                depthWrite: true,
-                alphaTest: 0,
-                opacity: 1,
-                visible: true,
-                side: THREE.FrontSide,
-                wireframe: false,
-            });
+        let geometry = new THREE.BoxGeometry(2, 2, 2);
+        let material = new THREE.MeshStandardMaterial({
+            color: 0x00ff00,
+            emissive: 0x0,
+            transparent: false,
+            opacity: 0,
+            depthTest: true,
+            depthWrite: true,
+            alphaTest: 0,
+            opacity: 1,
+            visible: true,
+            side: THREE.FrontSide,
+            wireframe: false,
+        });
         material.flatShading = false;
 
         let cube = new THREE.Mesh(geometry, material);
         this.scene.add(cube);
 
         let sunLight = new THREE.DirectionalLight(0xffffff, 3);
-        sunLight.position.set( - 1, 1.75, 1 );
+        sunLight.position.set(-1, 200, 1);
         sunLight.position.multiplyScalar(10)
         this.scene.add(sunLight)
 
-        let sunlightHelper = new THREE.DirectionalLightHelper(sunLight,5);
+        let sunlightHelper = new THREE.DirectionalLightHelper(sunLight, 5);
         this.scene.add(sunlightHelper);
         //sunLight.target = cube;
         //this.scene.add(envLight);
         this.loader = new GLTFLoader();
-        this.loader.load('static/model/test.glb',
-            (gltf)=>{
+        this.loader.load('static/model/scene.gltf',
+            (gltf) => {
                 let model = gltf.scene;
-                model.position.set(-800, -30, 70)
+                model.position.set(0, 0, 0)
                 this.scene.add(gltf.scene);
                 this.overlay.style.display = 'none';
-        }, (loading)=>{
-            let percent = loading.loaded / loading.total * 100
-            this.message.innerText = "Loading "+ percent.toFixed(0) + "%"
-            this.loadingBar.style.width = percent + '%';
-            if (percent == 100) {
-                this.message.innerText = "Rendering inprogress";
-            }
-        }, (error)=>{
-            console.log(error)
-        })
+            }, (loading) => {
+                let percent = loading.loaded / loading.total * 100
+                this.message.innerText = "Loading " + percent.toFixed(0) + "%"
+                this.loadingBar.style.width = percent + '%';
+                if (percent == 100) {
+                    this.message.innerText = "Rendering inprogress";
+                }
+            }, (error) => {
+                console.log(error)
+                this.overlay.style.display = 'none';
+            })
 
-        this.camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, .01, 300);
-        this.camera.position.set(5, 2, 8);
+        this.camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, .01, 900);
+        this.camera.position.set(100, 20, 8);
 
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.controls.target.set(0, 0.5, 0);
@@ -130,7 +141,7 @@ class Thesis {
         // }
 
         this.renderer.render(this.scene, this.camera);
-        
+
         this.controls.update();
 
         this.stats.update();
