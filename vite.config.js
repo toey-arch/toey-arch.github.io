301b6f70e741ddb75eaeef2d23837bf691483558
toey-arch <saranyatongmee.2000@gmail.com>
2024-04-07 16:11:51 +0700

    Test deploy with assets included

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
-const { resolve } = require('path')
+const {
+    resolve
+} = require('path')
 
 module.exports = {
-  build: {
-    rollupOptions: {
-      input: {
-        portfolio: resolve(__dirname, 'index.html'),
-        thesis: resolve(__dirname, './src/Thesis/index.html')
-      }
+    assetsInclude: ['./src/Thesis/static/**/*'],
+    build: {
+        rollupOptions: {
+            input: {
+                portfolio: resolve(__dirname, 'index.html'),
+                thesis: resolve(__dirname, './src/Thesis/index.html')
+            }
+        }
     }
-  }
 }
